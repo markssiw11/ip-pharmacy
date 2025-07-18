156df03c8ca50428dabe24d52d40128766baf541
long nguyen <long.nguyen@eastplayers.io>
2025-07-18 15:16:42 +0700

    update: change password

--- client/src/services/auth/auth.api.ts
+++ client/src/services/auth/auth.api.ts
@@ -1,5 +1,5 @@
 import request from "../axios";
-import { ILogin } from "./auth.type";
+import { IChangePasswordRequest, ILogin } from "./auth.type";
 
 const login = async (email: string, password: string) => {
   const res: IResponse<ILogin> = await request.post(
@@ -12,6 +12,23 @@ const login = async (email: string, password: string) => {
   return res.data;
 };
 
+const changePassword = async (data: IChangePasswordRequest) => {
+  try {
+    const res: IResponse<void> = await request.post(
+      "v1/auth/pharmacies/change-password",
+      {
+        current_password: data.currentPassword,
+        new_password: data.newPassword,
+        confirm_password: data.confirmPassword,
+      }
+    );
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 export const AuthApi = {
   login,
+  changePassword,
 };
